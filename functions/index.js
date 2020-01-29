7ef98df905217b3f48b9d312243602f14817990d
joaoh <joao_camargo10@hotmail.com>
2020-01-28 21:17:59 -0300

    setting custom claims

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,26 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase)
+
+exports.processSignUp = functions.auth.user().onCreate(user => {
+    const customClaims = {
+        "https://hasura.io/jwt/claims": {
+            "x-hasura-default-role": "user",
+            "x-hasura-allowed-roles": ["user"],
+            "x-hasura-user-id": user.uid
+        }
+    }
+
+    return admin
+        .auth()
+        .setCustomUserClaims(user.uid, customClaims)
+        .then(() => {
+            const metadataRef = admin.database().ref("metada/" + user.uid)
+
+            return metadataRef.set({ refreshTime: new Date().getTime() })
+        })
+        .catch(error => {
+            console.log(error)
+        })
+})
